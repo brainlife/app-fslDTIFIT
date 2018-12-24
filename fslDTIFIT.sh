85de52c659bb6d01d289478534c5c2f86ca4f9ee
Brad Caron <bacaron@imail.iu.edu>
2018-12-24 06:14:32 -0500

    changed to add mask capabilities and to get units into proper format

--- fslDTIFIT.sh
+++ fslDTIFIT.sh
@@ -5,55 +5,64 @@
 dwi=`jq -r '.dwi' config.json`;
 bvals=`jq -r '.bvals' config.json`;
 bvecs=`jq -r '.bvecs' config.json`;
+mask=`jq -r '.mask' config.json`;
 
 cp ${dwi} dwi.nii.gz;
 cp ${bvals} dwi.bvals;
 cp ${bvecs} dwi.bvecs;
 
-if [ -f "nodif_acpc_L1.nii.gz" ];then
-	echo "File exists. Skipping tensor fit"
-else
-	echo "Fitting tensor model"
-	## create b0 image
+if [ $mask == "null" ]; then
+    echo "creating brainmask"
+
+    ## create b0 image
 	select_dwi_vols \
 		dwi.nii.gz \
 		dwi.bvals \
-		nodif_acpc.nii.gz \
+		nodif.nii.gz \
 		0;
 
 	## Create mean b0 image
-	fslmaths nodif_acpc.nii.gz \
+	fslmaths nodif.nii.gz \
 		-Tmean \
-		nodif_acpc_mean;
+		nodif_mean;
 
 	## creates brainmask for DTIFIT
-	bet nodif_acpc_mean.nii.gz \
-		nodif_acpc_mean_brain \
+	bet nodif_mean.nii.gz \
+		nodif_mean_brain \
 		-f 0.4 \
 		-g 0 \
 		-m;
-
-	## fits tensor model
-	dtifit --data=dwi \
-		--out=nodif_acpc \
-		--mask=nodif_acpc_mean_brain_mask \
-		--bvecs=dwi.bvecs \
-		--bvals=dwi.bvals;
-
-	ret=$?
-	if [ ! $ret -eq 0 ]; then
-		echo "Tensor fit failed"
-		echo $ret > finished
-		exit $ret
-	fi
+else
+	cp ${mask} nodif_mean_brain_mask.nii.gz;
 fi
+
+echo "Fitting tensor model"
+
+## fits tensor model
+dtifit --data=dwi \
+	--out=nodif_acpc \
+	--mask=nodif_mean_brain_mask \
+	--bvecs=dwi.bvecs \
+	--bvals=dwi.bvals;
 
 fslmaths nodif_acpc_L2.nii.gz -add nodif_acpc_L3.nii.gz -div 2 rd.nii.gz;
 
 cp -v nodif_acpc_FA.nii.gz fa.nii.gz
 cp -v nodif_acpc_MD.nii.gz md.nii.gz
 cp -v nodif_acpc_L1.nii.gz ad.nii.gz;
+
+fslmaths md.nii.gz -mul 1000 md.nii.gz;
+fslmaths rd.nii.gz -mul 1000 rd.nii.gz;
+fslmaths ad.nii.gz -mul 1000 ad.nii.gz;
+
 rm -rf *nodif*;
 rm -rf *dwi.*;
+
+ret=$?
+if [ ! $ret -eq 0 ]; then
+	echo "Tensor fit failed"
+	echo $ret > finished
+	exit $ret
+fi
 
 echo "Tensor Fit Complete"
